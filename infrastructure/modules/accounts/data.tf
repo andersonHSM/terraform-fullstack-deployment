3fb54e55efd8ba00c926bc111490f8fd50edef52
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-17 12:04:52 -0300

    Update Terraform to include admin group membership and dependencies for SSO user provisioning

--- infrastructure/modules/accounts/data.tf
+++ infrastructure/modules/accounts/data.tf
@@ -1,12 +1,13 @@
 data "aws_ssoadmin_instances" "instances" {}
 
-data "aws_identitystore_user" "example" {
+data "aws_identitystore_user" "admin_user" {
   identity_store_id = tolist(data.aws_ssoadmin_instances.instances.identity_store_ids)[0]
+  depends_on        = [aws_identitystore_user.admin_user]
 
   alternate_identifier {
     unique_attribute {
       attribute_path  = "UserName"
-      attribute_value = "anderson_admin"
+      attribute_value = "andersonmenezes_admin"
     }
   }
 }
--- infrastructure/modules/accounts/main.tf
+++ infrastructure/modules/accounts/main.tf
@@ -1,19 +1,3 @@
-resource "aws_identitystore_user" "admin-user" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.instances.identity_store_ids)[0]
-
-  display_name = var.sso_admin_display_name_to_attach_to_account
-  user_name    = var.sso_admin_user_to_attach_to_account
-
-  name {
-    given_name  = var.sso_admin_name_to_attach_to_account
-    family_name = var.sso_admin_family_name_to_attach_to_account
-  }
-
-  emails {
-    value = var.sso_admin_email_to_attach_to_account
-  }
-}
-
 resource "aws_organizations_account" "account" {
   email                      = var.owner_email
   name                       = "deploy-${var.environment}"
@@ -33,6 +17,31 @@ resource "aws_identitystore_group" "admin_group" {
   identity_store_id = "Admin Group"
 }
 
+resource "aws_identitystore_user" "admin_user" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.instances.identity_store_ids)[0]
+
+  display_name = var.sso_admin_display_name_to_attach_to_account
+  user_name    = var.sso_admin_user_to_attach_to_account
+
+  name {
+    given_name  = var.sso_admin_name_to_attach_to_account
+    family_name = var.sso_admin_family_name_to_attach_to_account
+  }
+
+  emails {
+    value = var.sso_admin_email_to_attach_to_account
+  }
+
+  depends_on = [aws_organizations_account.account]
+}
+
+resource "aws_identitystore_group_membership" "assign_admin_to_group" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.instances.identity_store_ids)[0]
+  group_id          = aws_identitystore_group.admin_group.group_id
+  member_id         = aws_identitystore_user.admin_user.user_id
+  depends_on        = [aws_identitystore_user.admin_user]
+}
+
 resource "aws_ssoadmin_account_assignment" "basic_admin_account" {
   instance_arn       = tolist(data.aws_ssoadmin_instances.instances.arns)[0]
   permission_set_arn = aws_ssoadmin_permission_set.admin_access.arn
@@ -40,11 +49,11 @@ resource "aws_ssoadmin_account_assignment" "basic_admin_account" {
   principal_id   = aws_identitystore_group.admin_group.group_id
   principal_type = "GROUP"
 
-  target_id   = "123456789012"
+  target_id   = aws_organizations_account.account.id
   target_type = "AWS_ACCOUNT"
 }
 
-resource "aws_ssoadmin_managed_policy_attachment" "example" {
+resource "aws_ssoadmin_managed_policy_attachment" "admin_aws_managed_policy" {
   # Adding an explicit dependency on the account assignment resource will
   # allow the managed attachment to be safely destroyed prior to the removal
   # of the account assignment.
