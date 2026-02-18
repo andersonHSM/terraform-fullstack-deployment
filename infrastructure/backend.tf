aa0c447d6a58c6c20ce7908650a63757704ff4bf
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-18 13:30:08 -0300

    Update `backend.tf`: specify AWS provider version constraint

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.32"
+    }
+  }
   backend "s3" {
     region       = "us-east-1"
     bucket       = "roadmap-aws-infrastructure"
