e4b2b0b34176476cfa240e64ac944d06d555bd0d
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-20 08:52:32 -0300

    Add SSO instance and admin group data sources to IAM module

--- infrastructure/modules/iam/data.tf
+++ infrastructure/modules/iam/data.tf
@@ -1,3 +1,5 @@
+data "aws_ssoadmin_instances" "sso_instance" {}
+
 data "aws_iam_policy_document" "allow_terraform_user_to_assume_role_policy" {
   statement {
     effect  = "Allow"
@@ -11,4 +13,14 @@ data "aws_iam_policy_document" "allow_terraform_user_to_assume_role_policy" {
 
 data "aws_iam_policy" "aws_managed_s3_full_access" {
   arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+data "aws_identitystore_group" "admin_group" {
+  identity_store_id = data.aws_ssoadmin_instances.sso_instance.identity_store_ids[0]
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = ""
+      attribute_value = ""
+    }
+  }
 }
