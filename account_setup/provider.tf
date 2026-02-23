cf7b0b162bb1a684cc1d90846ee1b2d6eb01b81a
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-22 23:59:33 -0300

    Add Terraform backend configuration for QA/Prod environments and initialize provider setup

---
+++ account_setup/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.32"
+    }
+  }
+
+  backend "s3" {
+    use_lockfile = true
+    encrypt      = true
+
+  }
+}
+
