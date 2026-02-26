5358c68497b09d2464d88d7012979c0da9409406
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-26 17:50:42 -0300

    Adicionar configuração do módulo `code_build` em `ci_cd`, ajustar backend e incluir permissões IAM necessárias

---
+++ 3.ci_cd/modules/code_build/iam.tf
@@ -0,0 +1,91 @@
+data "aws_iam_policy_document" "code_build_assume_role" {
+  statement {
+    sid = "AllowCodeBuildToAssumeRole"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = ["codebuild.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "code_build_role_permissions" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:CreateNetworkInterfacePermission"]
+    resources = ["arn:aws:ec2:us-east-1:123456789012:network-interface/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "ec2:Subnet"
+
+      values = [
+        aws_subnet.example1.arn,
+        aws_subnet.example2.arn,
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "ec2:AuthorizedService"
+      values   = ["codebuild.amazonaws.com"]
+    }
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "codeconnections:GetConnectionToken",
+      "codeconnections:GetConnection"
+    ]
+    resources = ["arn:aws:codestar-connections:us-east-1:123456789012:connection/guid-string"]
+  }
+}
+
+
+resource "aws_iam_role" "code_build" {
+  assume_role_policy = data.aws_iam_policy_document.code_build_assume_role
+  name               = "code_build_role_${var.environment}"
+  path               = "/ci/"
+}
--- 3.ci_cd/modules/code_build/main.tf
+++ 3.ci_cd/modules/code_build/main.tf
@@ -1,1 +1,10 @@
+resource "aws_codebuild_project" "environment_builder" {
+  name         = "frontend_builder_${var.environment}"
+  service_role = aws_iam_role.code_build.arn
+  environment {
+    compute_type = ""
+    image        = ""
+    type         = ""
+  }
 
+}
