dd34ebedda612b7590b5e07c1d1dfd53e5d85e17
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-28 19:02:45 -0300

    Habilitar badge, configurar submódulos e definir `buildspec` no `code_build`.

--- 3.ci_cd/modules/code_build/main.tf
+++ 3.ci_cd/modules/code_build/main.tf
@@ -1,6 +1,8 @@
 resource "aws_codebuild_project" "frontend" {
-  name         = local.frontend_project_name
-  service_role = aws_iam_role.code_build.arn
+  name          = local.frontend_project_name
+  service_role  = aws_iam_role.code_build.arn
+  badge_enabled = true
+
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
@@ -17,6 +19,13 @@ resource "aws_codebuild_project" "frontend" {
   }
 
   source {
+    git_clone_depth = 1
+
+    git_submodules_config {
+      fetch_submodules = true
+    }
+
+    buildspec = ".config/buildspec.yml"
     auth {
       resource = var.repo_code_star_connection_arn
       type     = "CODECONNECTIONS"
