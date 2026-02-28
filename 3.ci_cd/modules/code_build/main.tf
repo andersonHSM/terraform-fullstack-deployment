1405812a59f4e907426adb420a68bc345a68c22f
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-28 19:10:57 -0300

    Atualizar imagem do CodeBuild para `standard:7.0` no módulo `code_build`.

--- 3.ci_cd/modules/code_build/main.tf
+++ 3.ci_cd/modules/code_build/main.tf
@@ -5,7 +5,7 @@ resource "aws_codebuild_project" "frontend" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
   }
