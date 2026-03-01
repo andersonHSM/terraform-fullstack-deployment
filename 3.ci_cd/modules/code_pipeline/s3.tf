c2dc88b07bcec7025df61368610eb17a5f5d1b6c
Anderson Menezes <andersonmenezes.dev@gmail.com>
2026-02-28 21:20:20 -0300

    Adicionar configuração de ciclo de vida no bucket S3 do `code_pipeline` para expiração de artefatos.

--- 3.ci_cd/modules/code_pipeline/s3.tf
+++ 3.ci_cd/modules/code_pipeline/s3.tf
@@ -18,3 +18,29 @@ resource "aws_s3_bucket_public_access_block" "code_pipeline" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "code_pipeline" {
+  bucket = aws_s3_bucket.code_pipeline.bucket
+  rule {
+
+    id     = "MonthlyPipelinedArtifactsExpriation"
+    status = "Enabled"
+    filter {
+      prefix = "/${aws_codepipeline.frontend.name}"
+    }
+    expiration {
+      days = 30
+    }
+    noncurrent_version_expiration {
+      noncurrent_days           = 30
+      newer_noncurrent_versions = 5
+    }
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+    noncurrent_version_transition {
+      noncurrent_days = 7
+      storage_class   = "GLACIER_IR"
+    }
+  }
+}
